31585ea53a92e16c923221fd8ebb3d1183325fd2
Xatouh <darlopezp@udd.cl>
2025-03-11 11:25:30 -0300

    Test STT linux

--- classNotes/app/processFile.py
+++ classNotes/app/processFile.py
@@ -1,7 +1,7 @@
 import buzz.transcriber
 from openai import OpenAI
 import subprocess
-
+import os
 
 
 def obtenerResumen(message):
@@ -20,38 +20,34 @@ def obtenerResumen(message):
     return completion.choices[0].message.content
 
 def STT(audio_file):
-    result = subprocess.run(["python","--version"], capture_output=True)
-    print(result)
+    my_env = os.environ.copy()
+    my_env["QT_QPA_PLATFORM"] = "offscreen"
 
+    route = "/home/xatou/projects/classNotes/src/Github/classNotes/classNotes/" + audio_file
     command = [
-        "python", "-m", "buzz", "add", 
+	"QT_QPA_PLATFORM=offscreen","/snap/bin/buzz", "add", 
         "-t", "transcribe", 
         "-l", "es", 
         "-m", "whisper", 
         "-s", "small", 
         "-p", "Transcribe este audio de una clase de informática, el nombre de la clase es métodos numéricos y es una clase introductoria",
         "--txt", 
-        audio_file
+        route
     ]
 
     # Set environment variables
-    env = {"QT_QPA_PLATFORM": "offscreen"}
-
+    
+    print("executing:", " ".join(command))
     # Execute the command
     result = subprocess.run(
-        command,
-        env=env,
-        capture_output=True,
-        text=True
+        " ".join(command),
+	shell=True,
+	env=my_env,
+	capture_output=True,
+	
     )
-
+    
     # Check if command was successful
-    if result.returncode == 0:
-        print("Command executed successfully")
-        print("Output:", result.stdout)
-    else:
-        print("Command failed")
-        print("Error:", result.stderr)
-
+    print(result)
     return result
 
